e913e8fad79c45650c1be334ae41af6f270464f4
Vic Wintriss <vic@wintrisstech.org>
2023-05-17 21:41:20 -0700

    * version 230517 Disadvantage going west

--- src/main/java/org/wintrisstech/CityNameMapBuilder.java
+++ src/main/java/org/wintrisstech/CityNameMapBuilder.java
@@ -1,57 +1,56 @@
 package org.wintrisstech;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 public class CityNameMapBuilder
 {
-    private static HashMap<String, String> cityNameMap = new LinkedHashMap<String, String>();
+    private static Map<String, String> cityNameMap = new LinkedHashMap<String, String>();
     public CityNameMapBuilder()
     {
         System.out.println("CityNameMapBuilder...building cityNameMap");
-        cityNameMap.put("Minneapolis", "Minnesota");//Minnesota Vikings
-        cityNameMap.put("Tampa", "Tampa Bay");//Tampa Bay Buccaneers
-        cityNameMap.put("Tampa Bay", "Tampa Bay");//Tampa Bay Buccaneers
-        cityNameMap.put("Arlington", "Dallas");//Dallas Cowboys
-        cityNameMap.put("Dallas", "Dallas");//Dallas Cowboys
-        cityNameMap.put("Orchard Park", "Buffalo");//Buffalo Bills
-        cityNameMap.put("Buffalo", "Buffalo");//Buffalo Bills
-        cityNameMap.put("Charlotte", "Carolina");//Carolina Panthers
-        cityNameMap.put("Carolina", "Carolina");//Carolina Panthers
-        cityNameMap.put("Arizona", "Arizona");//Arizona Cardinals
-        cityNameMap.put("Tempe", "Arizona");//Arizona Cardinals
-        cityNameMap.put("Foxborough", "New England");//New England Patriots
-        cityNameMap.put("New England", "New England");//New England Patriots
-        cityNameMap.put("East Rutherford", "New York");//New York Giants and New York Jets
-        cityNameMap.put("New York", "New York");//New York Giants and New York Jets
-        cityNameMap.put("Landover", "Washington");//Washington Football Team
-        cityNameMap.put("Washington", "Washington");//Washington Football Team
-        cityNameMap.put("Nashville", "Tennessee");//Tennessee Titans
-        cityNameMap.put("Miami", "Miami");//Miami Dolphins
-        cityNameMap.put("Baltimore", "Baltimore");//Baltimore Ravens
-        cityNameMap.put("Cincinnati", "Cincinnati");//Cincinnati Bengals
-        cityNameMap.put("Cleveland", "Cleveland");//Cleveland Browns
-        cityNameMap.put("Pittsburgh", "Pittsburgh");//Pittsburgh Steelers
-        cityNameMap.put("Houston", "Houston");//Houston Texans
-        cityNameMap.put("Indianapolis", "Indianapolis");//Indianapolis Colts
-        cityNameMap.put("Jacksonville", "Jacksonville");//Jacksonville Jaguars
-        cityNameMap.put("Tennessee", "Tennessee");//Tennessee Titans
-        cityNameMap.put("Denver", "Denver");//Denver Broncos
-        cityNameMap.put("Kansas City", "Kansas City");//Kansas City Chiefs
-        cityNameMap.put("Las Vegas", "Las Vegas");//Los Angeles Chargers and Los Angeles Rams
-        cityNameMap.put("Philadelphia", "Philadelphia");//Philadelphia Eagles
-        cityNameMap.put("Chicago", "Chicago");//Chicago Bears
-        cityNameMap.put("Detroit", "Detroit");//Detroit Lions
-        cityNameMap.put("Green Bay", "Green Bay");//Green Bay Packers
-        cityNameMap.put("Minnesota", "Minnesota");
-        cityNameMap.put("Atlanta", "Atlanta");//Atlanta Falcons
-        cityNameMap.put("New Orleans", "New Orleans");//New Orleans Saints
-        cityNameMap.put("Los Angeles", "Los Angeles");//Los Angeles Rams
-        cityNameMap.put("San Francisco", "San Francisco");//San Francisco 49ers
-        cityNameMap.put("Seattle", "Seattle");//Seattle Seahawks
-        System.out.println(cityNameMap.size() + " teams");
+        cityNameMap.put("Minneapolis", "1&Minnesota");//Minnesota Vikings
+        cityNameMap.put("Tampa", "2&Tampa Bay");//Tampa Bay Buccaneers
+        cityNameMap.put("Tampa Bay", "2&Tampa Bay");//Tampa Bay Buccaneers
+        cityNameMap.put("Arlington", "3&Dallas");//Dallas Cowboys
+        cityNameMap.put("Dallas", "3&Dallas");//Dallas Cowboys
+        cityNameMap.put("Orchard Park", "4&Buffalo");//Buffalo Bills
+        cityNameMap.put("Buffalo", "4&Buffalo");//Buffalo Bills
+        cityNameMap.put("Charlotte", "5&Carolina");//Carolina Panthers
+        cityNameMap.put("Carolina", "5&Carolina");//Carolina Panthers
+        cityNameMap.put("Arizona", "6&Arizona");//Arizona Cardinals
+        cityNameMap.put("Tempe", "6&Arizona");//Arizona Cardinals
+        cityNameMap.put("Foxborough", "7&New England");//New England Patriots
+        cityNameMap.put("New England", "7&New England");//New England Patriots
+        cityNameMap.put("East Rutherford", "8&New York");//New York Giants and New York Jets
+        cityNameMap.put("New York", "8&New York");//New York Giants and New York Jets
+        cityNameMap.put("Landover", "9&Washington");//Washington Football Team
+        cityNameMap.put("Washington", "9&Washington");//Washington Football Team
+        cityNameMap.put(" Nashville", "10&Tennessee");//Tennessee Titans
+        cityNameMap.put("Miami", "11&Miami");//Miami Dolphins
+        cityNameMap.put("Baltimore", "12&Baltimore");//Baltimore Ravens
+        cityNameMap.put("Cincinnati", "13&Cincinnati");//Cincinnati Bengals
+        cityNameMap.put("Cleveland", "14&Cleveland");//Cleveland Browns
+        cityNameMap.put("Pittsburgh", "15&Pittsburgh");//Pittsburgh Steelers
+        cityNameMap.put("Houston", "16&Houston");//Houston Texans
+        cityNameMap.put("Indianapolis", "17&Indianapolis");//Indianapolis Colts
+        cityNameMap.put("Jacksonville", "18&Jacksonville");//Jacksonville Jaguars
+        cityNameMap.put("Tennessee", "19&Tennessee");//Tennessee Titans
+        cityNameMap.put("Denver", "20&Denver");//Denver Broncos
+        cityNameMap.put("Kansas City", "21&Kansas City");//Kansas City Chiefs
+        cityNameMap.put("Las Vegas", "22&Las Vegas");//Los Angeles Chargers and Los Angeles Rams
+        cityNameMap.put("Philadelphia", "23&Philadelphia");//Philadelphia Eagles
+        cityNameMap.put("Chicago", "24&Chicago");//Chicago Bears
+        cityNameMap.put("Detroit", "25&Detroit");//Detroit Lions
+        cityNameMap.put("Green Bay", "26&Green Bay");//Green Bay Packers
+        cityNameMap.put("Minnesota", "27&Minnesota");
+        cityNameMap.put("Atlanta", "28&Atlanta");//Atlanta Falcons
+        cityNameMap.put("New Orleans", "29&New Orleans");//New Orleans Saints
+        cityNameMap.put("Los Angeles", "30&Los Angeles");//Los Angeles Rams
+        cityNameMap.put("San Francisco", "31&San Francisco");//San Francisco 49ers
+        cityNameMap.put("Seattle", "32&Seattle");//Seattle Seahawks
+        System.out.println(cityNameMap.size() + " NFL teams");
         //cityNameMap.forEach((key, value) -> System.out.println(key + " = " + value));
     }
-    Map getCityNameMap()
+    static Map getCityNameMap(String bigNFLawayCityName)
     {
         return cityNameMap;
     }
--- src/main/java/org/wintrisstech/Main.java
+++ src/main/java/org/wintrisstech/Main.java
@@ -3,27 +3,29 @@ package org.wintrisstech;
  * Must be run before Selenium for initial setup
  * cd /usr/bin/
  * sudo safaridriver --enable
- * version 230429
+ * version 230517
+ * Teams going west have a circadian disadvantage
  **********************************************************************************/
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-
 import java.io.*;
+
 public class Main
 {
     private static XSSFWorkbook bigNFLWorkbook = new XSSFWorkbook();
     private static XSSFWorkbook book1 = new XSSFWorkbook();
     private static XSSFSheet sheet1 = book1.createSheet("Sheet1");
-    private static XSSFRow row;
-    private static String version = "version 230429";
+    private static String version = "version 230517";
     static String deskTopPath = System.getProperty("user.home") + "/Desktop";/* User's desktop path */
     private static double homeTeamScore;
     private static double awayTeamScore;
     private static XSSFSheet bigNFLSheet;
     private static FileOutputStream os;
+    private static String homeTeamName;
+    private static String homeTeamNumber;
     public static void main(String[] args) throws IOException, InterruptedException, InvalidFormatException
     {
         System.out.println("SharpMarkets, version " + version + ", Copyright 2023 Dan Farris");
@@ -40,36 +42,40 @@ public class Main
         {
             System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>Main65...problems reading " + deskTopPath + "/BigNFL.xlsx" + " sheet");
         }
+
         System.out.println("Successfully read...BigNFL Sheet, Size => " + bigNFLSheet.getLastRowNum());
-        //<START MAIN LOOP> >************************************************************************************************************ <START MAIN LOOP> *************************************************************************************************************
-        System.out.println("Main69 START MAIN LOOP ********** BigNFL Sheet Size =>  *********************** START MAIN LOOP");
+        System.out.println("Main69 START MAIN LOOP ********************************* START MAIN LOOP");
+        sheet1.createRow(0).createCell(11).setCellValue("Home Team Abbreviation");
         for (int i = 3; i < bigNFLSheet.getLastRowNum(); i++)
-        {
-            row = sheet1.createRow(i);
-            row.createCell(50);
-            homeTeamScore = bigNFLSheet.getRow(i).getCell(20).getNumericCellValue();// Home Team Score(Zero based)
-            awayTeamScore = bigNFLSheet.getRow(i).getCell(31).getNumericCellValue();// Away Team Score Zero based
-            if (homeTeamScore > awayTeamScore)
+        {//Start of main loop
+            String teamAbbreviation = bigNFLSheet.getRow(i).getCell(11).getStringCellValue();//Home Team Abbreviation
+            //sheet1.createRow(i).createCell(11).setCellValue(teamAbbreviation);//Home Team Abbreviation
+            homeTeamScore = bigNFLSheet.getRow(i).getCell(20).getNumericCellValue();//Home Team Score
+            awayTeamScore = bigNFLSheet.getRow(i).getCell(31).getNumericCellValue();//Away Team Score
+            String teamNames = bigNFLSheet.getRow(i).getCell(0).getStringCellValue().split("-")[1];//after removing date "-" split e.g. Jacksonville Jaguars @ Washington Commanders
+            String awayTeamName = teamNames.split("@")[0];//e.g.Jacksonville Jaguars
+            String awayCityName = awayTeamName.split(" ")[0];//e.g. Jacksonville
+            String homeTeamName = teamNames.split("@")[1];//e.g Cleveland Browns
+            String homeCityName = homeTeamName.split(" ")[0];//e.g. Cleveland
+        if (homeTeamScore > awayTeamScore)
             {
-                String homeWin = "1";
-                sheet1.getRow(i).getCell(50).setCellValue(homeWin);
+                System.out.println("Main64 Home Team Wins => " + homeCityName);
             }
             if (awayTeamScore > homeTeamScore)
             {
-                String awayWin = "0";
-                sheet1.getRow(i).getCell(50).setCellValue(awayWin);
+                System.out.println("Main68 Away Team Wins => " + awayCityName);
             }
-            System.out.println("========> " + sheet1.getRow(i).getCell(50).getStringCellValue());
-        }
-      try //Writing Book1.xlsx
-      {
+        }//End of main loop
+        System.out.println("Main69 END MAIN LOOP ********************************* END MAIN LOOP");
+        try //Writing Book1.xlsx...desired output
+        {
               os = new FileOutputStream(deskTopPath + "/Book1.xlsx");
               book1.write(os);
               os.close();
-          }
-          catch (Exception e)
-          {
-              System.out.println("Main91...problems writing " + deskTopPath + "/Book1.xlsx" + " sheet");
-          }
+        }
+        catch (Exception e)
+        {
+            System.out.println("Main91...problems writing " + deskTopPath + "/Book1.xlsx" + " sheet");
+        }
     }
 }
