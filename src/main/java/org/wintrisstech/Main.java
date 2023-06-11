076b2fbb621c2cde836dba85eac163bbdac59eb6
Vic Wintriss <vic@wintrisstech.org>
2023-06-11 16:31:53 -0700

    version 230611 Added home/away team numbers and wn team number...

--- src/main/java/org/wintrisstech/Main.java
+++ src/main/java/org/wintrisstech/Main.java
@@ -1,6 +1,6 @@
 package org.wintrisstech;
 /**********************************************************************************
- * version 230610B
+ * version 230611
  * Teams going west have a circadian disadvantage
  **********************************************************************************/
 
@@ -10,25 +10,30 @@ import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Arrays;
+
 import static org.wintrisstech.BigNFLbookReader.bigNFLsheet;
 import static org.wintrisstech.CityNameMapBuilder.cityNameMap;
+import static org.wintrisstech.CityNameMapBuilder.homeCity;
+
 public class Main {
     private static XSSFWorkbook book1 = new XSSFWorkbook();//Output workbook
     private static XSSFSheet sheet1 = book1.createSheet("Data");//Output sheet
-    private static String version = "version 230610B";
+    private static String version = "version 230611";
     static String deskTopPath = System.getProperty("user.home") + "/Desktop";/* User's desktop path */
     private static double homeScore;
     private static double awayScore;
     private static FileOutputStream os;
     public static String homeNumber;
     public static String awayNumber;
-    public static String homecity;
+    public static String homeCity;
     public static String cityName;
     public static String winCity;
     private static String awayCity;
     private static String[] homeNameArray;
     private static String[] awayNameArray;
     private static String cityNumber;
+    private static String homeZone;
 
     public static void main(String[] args) throws IOException, InterruptedException, InvalidFormatException {
         new BigNFLbookReader();
@@ -36,16 +41,26 @@ public class Main {
         cityNameMap = CityNameMapBuilder.cityNameMap;
         bigNFLsheet = BigNFLbookReader.bigNFLsheet;
         System.out.println("SharpMarkets, version " + version + ", Copyright 2023 Dan Farris");
-        System.out.println("Main37 START MAIN LOOP ***************************BigNFLsheet size: " + bigNFLsheet.getLastRowNum() + "**************************************** START MAIN LOOP");
+        if (sheet1.getRow(0) == null)//If the row doesn't exist, create it
+        {
+            sheet1.createRow(0);
+        }
+        sheet1.getRow(0).createCell(0).setCellValue("Home Team");
+        sheet1.getRow(0).createCell(1).setCellValue("Away Team");
+        sheet1.getRow(0).createCell(2).setCellValue("Delta Time");
+        sheet1.getRow(0).createCell(3).setCellValue("Win Team");
+        System.out.println("*****************Main37 START MAIN LOOP ***************************BigNFLsheet size: " + bigNFLsheet.getLastRowNum() + "**************************************** START MAIN LOOP");
         for (int i = 3; i < bigNFLsheet.getLastRowNum(); i++)//Start of main loop interating through BigNFL.xlsx...all games in history
         {
-            System.out.print("=======> row[" + i + "] ...");
+            System.out.print("row[" + i + "] ...");
             homeNameArray = bigNFLsheet.getRow(i).getCell(10).getStringCellValue().split(" ");//Home Team Name
             awayNameArray = bigNFLsheet.getRow(i).getCell(21).getStringCellValue().split(" ");//Away Team Name
-            homecity = getCleanCityName(homeNameArray);
+            homeCity = getCleanCityName(homeNameArray);
             awayCity = getCleanCityName(awayNameArray);
-            homeNumber = getCityNumber(homecity);
+            homeNumber = getCityNumber(homeCity);
             awayNumber = getCityNumber(awayCity);
+            //homeZone = Arrays.toString(cityNameMap.get(homeCity).split(" "));//Home Team Time Zone
+            System.out.println("..................................................homeCity: " + homeCity);
             homeScore = bigNFLsheet.getRow(i).getCell(20).getNumericCellValue();//Home Team Score
             awayScore = bigNFLsheet.getRow(i).getCell(31).getNumericCellValue();//Away Team Score
             if (homeScore > awayScore)//Home Team Wins, so put home team number in the cell 6
@@ -80,7 +95,7 @@ public class Main {
             sheet1.getRow(i).createCell(0).setCellValue(homeNumber);//Put home team number in the cell 0
             sheet1.getRow(i).createCell(1).setCellValue(awayNumber);//Put away team number in the cell 1
         }//End of main loop
-        System.out.println("Main63 END MAIN LOOP **********************************" + bigNFLsheet.getLastRowNum() + "**************************************************** END MAIN LOOP");
+        System.out.println("****************END MAIN LOOP **********************************" + bigNFLsheet.getLastRowNum() + "**************************************************** END MAIN LOOP");
         try //Writing Book1.xlsx...epoch
         {
             System.out.println("Main69...Writing " + deskTopPath + "/Book1.xlsx" + " sheet");
@@ -93,7 +108,7 @@ public class Main {
     }
 
     private static void printWin() {
-        System.out.println("#" + awayNumber + " " + awayCity + ":" + (int) awayScore + " @ " + "#" + homeNumber + " " + homecity + ":" + (int) homeScore);
+        System.out.println("#" + awayNumber + " " + awayCity + ":" + (int) awayScore + " @ " + "#" + homeNumber + " " + homeCity + ":" + (int) homeScore);
     }
 
     private static String getCleanCityName(String[] teamNameArray)//
